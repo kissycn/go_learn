3925f55a0abc1361a44d760c73fd11058359b517
kissy <kissycn@gmail.com>
2022-07-26 22:55:04 +0800

    feat: leetcode by three sums to solve by two index solution

--- chapter17_data_struct/array_list/array/three_sums_test.go
+++ chapter17_data_struct/array_list/array/three_sums_test.go
@@ -2,16 +2,51 @@ package array
 
 import (
 	"fmt"
+	"sort"
 	"testing"
 )
 
 func TestThreeSums(t *testing.T) {
-	nums := []int{-1, 0, 1, 2, -1, -4}
+	nums := []int{-2, 0, 0, 2, 2}
 	fmt.Println(threeSum(nums))
 }
 
-// 两数之和，采用hash求解
 func threeSum(nums []int) [][]int {
+	res := [][]int{}
+	sort.Ints(nums)
+	for k := 0; k < len(nums)-2; k++ {
+		if nums[k] > 0 {
+			//若nums[k]大于0，则后面的数字也是大于零（排序后是递增的）
+			break
+		}
+		if k > 0 && nums[k] == nums[k-1] {
+			continue //nums[k]值重复了，去重
+		}
+		left, right := k+1, len(nums)-1
+		for left < right {
+			sum := nums[left] + nums[right] + nums[k]
+			if sum > 0 {
+				right--
+			} else if sum < 0 {
+				left++
+			} else {
+				res = append(res, []int{nums[left], nums[right], nums[k]})
+				for right > left && nums[right] == nums[right-1] {
+					right--
+				}
+				for right > left && nums[left] == nums[left+1] {
+					left++
+				}
+				left++
+				right--
+			}
+		}
+	}
+	return res
+}
+
+// 两数之和，采用hash求解
+func threeSum2(nums []int) [][]int {
 	res := [][]int{}
 	m := map[int][]int{}
 	for i := 0; i < len(nums)-2; i++ {
