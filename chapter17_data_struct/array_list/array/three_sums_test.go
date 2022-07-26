f037cf54de118206470df50b3dbeff0910b01751
kissy <kissycn@gmail.com>
2022-07-26 21:55:17 +0800

    feat: leetcode by three sums to solve by hash solution

--- chapter17_data_struct/array_list/array/three_sums_test.go
+++ chapter17_data_struct/array_list/array/three_sums_test.go
@@ -10,8 +10,33 @@ func TestThreeSums(t *testing.T) {
 	fmt.Println(threeSum(nums))
 }
 
-// 暴力求解法，三层遍历实践复杂度为：O(n^3)
+// 两数之和，采用hash求解
 func threeSum(nums []int) [][]int {
+	res := [][]int{}
+	m := map[int][]int{}
+	for i := 0; i < len(nums)-2; i++ {
+		for j := i + 1; j < len(nums)-1; j++ {
+			c := 0 - nums[i] - nums[j]
+			if val, ok := m[c]; ok {
+				res = append(res, val)
+			} else {
+				m[c] = []int{nums[i], nums[j], c}
+			}
+			// a + b + c = 0 ----> a + b = -c
+			//if val, ok := m[-(nums[i] + nums[j])]; ok {
+			//	res = append(res, val)
+			//} else {
+			//	// a + b + c = 0 ----> c = 0 - a - b
+			//	c := 0 - nums[i] - nums[j]
+			//	m[c] = []int{nums[i], nums[j], c}
+			//}
+		}
+	}
+	return res
+}
+
+// 暴力求解法，三层遍历实践复杂度为：O(n^3)
+func threeSum1(nums []int) [][]int {
 	res := [][]int{}
 	for i := 0; i < len(nums)-2; i++ {
 		for j := i + 1; j < len(nums)-1; j++ {
